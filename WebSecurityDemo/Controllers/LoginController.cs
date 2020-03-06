48008610d90facda31b45b4a19eb111e8f2c5b10
giridharan <giridharan2198@gmail.com>
2020-03-06 17:08:58 +0530

    Add project files.

---
+++ WebSecurityDemo/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebSecurityDemo.Models;
+namespace WebSecurityDemo.Content
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginInfo x) {
+            if (ModelState.IsValid && x.UN=="abc"&& x.PW == "abc") {
+                FormsAuthentication.SetAuthCookie(x.UN, false);
+                return RedirectToAction("Index", "Home");
+            }
+            
+            else {
+                return View(x);
+            }
+        }
+
+    }
+}
